3213a06b2ed99bd528d27ce329c1786f323af6ad
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-12-03 15:57:46 -0800

    created middleware to handle errors

--- server/index.js
+++ server/index.js
@@ -5,18 +5,15 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 
 const app = express();
-app.use(express.json())  // this will allow json as the input of the backend
+app.use(express.json()); // this will allow json as the input of the backend
 
 dotenv.config({ path: "../.env" });
 
-const mongo_url = process.env.MONGO
-const port = process.env.PORT || 3000
-
+const mongo_url = process.env.MONGO;
+const port = process.env.PORT || 3000;
 
 mongoose
-  .connect(
-    mongo_url
-  )
+  .connect(mongo_url)
   .then(() => {
     console.log("mongodb is connected");
   })
@@ -24,11 +21,19 @@ mongoose
     console.log(err);
   });
 
-
 app.listen(3000, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});

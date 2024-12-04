4e41dff374952b99fd11adab4e0141c7079c8ec3
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-12-04 11:03:57 -0800

    edit to signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className="min-h-screen mt-20">
+      {/* left */}
+      <div className="">
+        <Link to="/" className="whitespace-nowrap font-bold dark:text-white text-4xl">
+          <span className="px-2 py-1 bg-green-400 rounded-lg text-white">
+            Blog About It!
+          </span>
+        </Link>
+      </div>
+
+      {/* right */}
+      <div className=""></div>
+    </div>
+  );
 }

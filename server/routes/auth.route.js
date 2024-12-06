2fde7eaf710931c107d5b01791f7e9325118b678
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-12-06 11:56:21 -0800

    added google OAuth functionality

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { signup, signin } from "../controllers/auth.controller.js";
+import { signup, signin, google } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-
+// the functions are built in the controllers directory then imported into this file.
+// helps keep the code clean.
 router.post("/signup", signup);
 router.post("/signin", signin);
+router.post("/google", google)
 
 export default router;

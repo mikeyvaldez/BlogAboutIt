aa77a05182503a206fea5c45976db0ea84cc86b6
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-12-16 22:30:59 -0800

    added posts section to the dashboard

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -78,7 +78,7 @@ export default function CreatePost() {
       if (res.ok) {
         setPublishError(null);
         navigate(`/post/${data.slug}`);
-      }
+      } 
     } catch (error) {
       setPublishError("Something went wrong");
     }
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DashboardSidebar from "../components/DashboardSidebar";
 import DashboardProfile from "../components/DashboardProfile";
+import DashboardPosts from "../components/DashboardPosts";
 
 export default function Dashboard() {
   const location = useLocation();
@@ -25,6 +26,8 @@ export default function Dashboard() {
       </div>
       {/* profile */}
       {tab === "profile" && <DashboardProfile />}
+      {/* posts... */}
+      {tab === "posts" && <DashboardPosts />}
     </div>
   )
 }

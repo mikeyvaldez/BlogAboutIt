4b841ac73f31b3cec0edd9466eb60558c2ff2c81
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-12-17 22:41:40 -0800

    started show user posts in dashboard

--- client/src/components/DashboardPosts.jsx
+++ client/src/components/DashboardPosts.jsx
@@ -1,7 +1,38 @@
-
+import { Table } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 export default function DashboardPosts() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
+  console.log(userPosts)
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const data = await res.json()
+        if(res.ok){
+          setUserPosts(data.posts)
+        }
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+    if(currentUser.isAdmin){
+      fetchPosts();
+    }
+  }, [currentUser._id]);
+
   return (
-    <div>DashboardPosts</div>
+    <div>
+      {currentUser.isAdmin && userPosts.length > 0 ? (
+        <>
+        <Table hoverable className="shadow-md"></Table>
+        </>
+      ):(
+        <p>You have no posts yet!</p>
+      ) }
+    </div>
   )
 }

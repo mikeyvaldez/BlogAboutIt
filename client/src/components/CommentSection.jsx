2f64c46bb8af1701a57c019b34c2de9a1f2a5191
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-12-28 22:06:01 -0800

    show the comments of a post

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,45 +1,68 @@
 import { Alert, Button, Textarea } from "flowbite-react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Comment from "./Comment";
+import PropTypes from "prop-types";
 
 export default function CommentSection({ postId }) {
   const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState("");
   const [commentError, setCommentError] = useState(null);
-
+  const [comments, setComments] = useState([]);
+  console.log(comments);  
+  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if(comment.length > 200){
+    if (comment.length > 200) {
       return;
     }
 
     try {
-      const res = await fetch('/api/comment/create', {
+      const res = await fetch("/api/comment/create", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ content: comment, postId, userId: currentUser._id }),
+        body: JSON.stringify({
+          content: comment,
+          postId,
+          userId: currentUser._id,
+        }),
       });
-  
+
       const data = await res.json();
-      
-      if(res.ok){
+
+      if (res.ok) {
         setComment("");
         setCommentError(null);
+        setComments([data, ...comments]);
       }
     } catch (error) {
-      setCommentError(error.message)      
+      setCommentError(error.message);
     }
-  }
-    
+  };
+
+  useEffect(() => {
+    const getComments = async () => {
+      try {
+        const res = await fetch(`/api/comment/getPostComments/${postId}`);
+        if (res.ok) {
+          const data = await res.json();
+          setComments(data);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getComments();
+  }, [postId]);
 
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
-        <div className="flex items-center gap-1 my-5 text-gray-500 text-sm">
+        <div className="flex items-center gap-2 my-5 text-gray-500 text-sm">
           <p>Signed in as:</p>
           <img
             className="h-5 w-5 object-cover rounded-full"
@@ -62,19 +85,56 @@ export default function CommentSection({ postId }) {
         </div>
       )}
       {currentUser && (
-        <form onSubmit={handleSubmit} className="border border-teal-500 rounded-md p-3">
-          <Textarea placeholder="Add a comment..." rows="3" maxLength="200" onChange={(e) => setComment(e.target.value)} value={comment} />
+        <form
+          onSubmit={handleSubmit}
+          className="border border-teal-500 rounded-md p-3"
+        >
+          <Textarea
+            placeholder="Add a comment..."
+            rows="3"
+            maxLength="200"
+            onChange={(e) => setComment(e.target.value)}
+            value={comment}
+          />
           <div className="flex justify-between items-center mt-5">
-            <p className="text-gray-500 text-xs">{200 - comment.length} characters remaining</p>
+            <p className="text-gray-500 text-xs">
+              {200 - comment.length} characters remaining
+            </p>
             <Button outline gradientDuoTone="greenToBlue" type="submit">
-                Submit
+              Submit
             </Button>
           </div>
           {commentError && (
-            <Alert color="failure" className="mt-5">{commentError}</Alert>
+            <Alert color="failure" className="mt-5">
+              {commentError}
+            </Alert>
           )}
         </form>
       )}
+      {comments.length === 0 ? (
+        <p className="text-sm my-5">No comments yet!</p>
+      ) : (
+        <>
+          <div className="text-sm my-5 flex items-center gap-1">
+            <p>Comments</p>
+            <div className="border border-gray-400 py-1 px-2 rounded-sm">
+              <p>{comments.length}</p>
+            </div>
+          </div>
+          {
+            comments.map(comment => (
+              <Comment
+                key={comment._id}
+                comment={comment}
+              />
+            ))
+          }
+        </>
+      )}
     </div>
   );
+}
+
+CommentSection.propTypes = {
+  postId: PropTypes.string,
 }
--- server/models/comment.model.js
+++ server/models/comment.model.js
@@ -1,24 +1,31 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const commentSchema = new mongoose.Schema({
+const commentSchema = new mongoose.Schema(
+  {
     content: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+    },
+    postId: {
+      type: String,
+      required: true,
     },
     userId: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     likes: {
-        type: Array,
-        default: [],
+      type: Array,
+      default: [],
     },
     numberOfLikes: {
-        type: Number,
-        default: 0,
-    },    
-}, { timestamps: true });
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
 
-const Comment = mongoose.model("Comment", commentSchema);
+const Comment = mongoose.model('Comment', commentSchema);
 
 export default Comment;
